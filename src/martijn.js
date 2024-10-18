b80c2c44ab82795c99b771e04fe823fd47fce8d0
Martijn <65791391+BoorZuur@users.noreply.github.com>
2024-10-18 15:19:29 +0200

    begin heartrate

--- src/martijn.js
+++ src/martijn.js
@@ -1,33 +1,32 @@
-let minutesAmount = 0;
+let measurements: number[] = []
+let currentRate = 0;
+let averageRate = 0;
+let beats = 0;
+let bpm = 0;
 
-// CPE at door
-input.buttonsAB.onEvent(ButtonEvent.Click, function () {
-    network.infraredSendNumber(minutesAmount*10)
-})
+light.setBrightness(255);
+light.setPixelColor(1, Colors.Green);
 
-input.buttonA.onEvent(ButtonEvent.Click, function () {
-    minutesAmount--;
-    checkValidMinutes();
-    light.setPixelColor(minutesAmount, Colors.Black);
-})
-
-input.buttonB.onEvent(ButtonEvent.Click, function () {
-    minutesAmount++;
-    checkValidMinutes();
-    light.setPixelColor(minutesAmount-1, Colors.Orange);
-})
-
-
-function checkValidMinutes() {
-    if (minutesAmount < 0) {
-        minutesAmount = 0;
+forever(function () {
+    currentRate = input.lightLevel();
+    measurements.push(currentRate);
+    calculateAverage()
+    pause(50)
+    logger()
+    if (measurements.length > 20) {
+        measurements.removeAt(0)
     }
-    if (minutesAmount > 10) {
-        minutesAmount = 10;
-    }
+});
+
+function logger() {
+    console.log(`Current: ${currentRate}`)
+    console.log(`Average: ${averageRate}`)
 }
 
-// CPE at desk or outside room
-network.onInfraredReceivedNumber(function (num) {
-    light.setAll(Colors.Orange)
-})
+function calculateAverage() {
+    let sum = 0;
+    for (let i = 0; i < measurements.length; i++) {
+        sum += measurements[i];
+    }
+    averageRate = sum / measurements.length;
+}
