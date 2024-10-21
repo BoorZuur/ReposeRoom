35f3297798df325afca03b73a72909465a94f520
OliviaRijkes <1101595@hr.nl>
2024-10-21 13:34:30 +0200

    Update IRtimer.js

--- src/IRtimer.js
+++ src/IRtimer.js
@@ -1,40 +1,40 @@
 let room = 0
-let timer = 0
-network.infraredSendNumber(0)
+let closed=false
+//network.onInfraredReceivedNumber(function (num: number) {room=num})
+network.infraredSendNumber(0)// turns the other playground on to recieve.
 input.buttonA.onEvent(ButtonEvent.Click, function () {
     room++
     light.setPixelColor(room - 1, 0x00ff00)
-    network.infraredSendNumber(110)
 })
 input.buttonB.onEvent(ButtonEvent.Click, function () {
     room--
     light.setPixelColor(room, 0)
-    network.infraredSendNumber(101)
 })
 input.buttonsAB.onEvent(ButtonEvent.Click, function () {
-    timer = room
+    // detects if the room is dark, before making light with the heartRateLed
+    let timer = room
+    network.infraredSendNumber(timer)
     if (input.lightLevel() < 125) {
         pins.A0.analogWrite(1023)
     }
+    // starts timer
     network.infraredSendNumber(111)
+    closed=true
     crickit.servo1.setAngle(90)
     for (let i = 0; i < timer; i++) {
         room--
         pause(100)
         light.setPixelColor(room, 0xff0000)
-        network.infraredSendNumber(101)
     }
     console.log(room)
     network.infraredSendNumber(100)
     crickit.servo1.setAngle(0)
     pins.A0.analogWrite(0)
 })
-
-// de eerste is de kamer nummer => 1 =room1
-//geef 100= room available
-//geef 111= room not available
-//geef 110=room +1
-//geef 101=room-1
+//reciever
+//100= room available
+//111= room not available
+//not= roomtimer
 let roomtimer = 0
 network.onInfraredReceivedNumber(function (num: number) {
     if (num === 100) {
@@ -45,13 +45,13 @@ network.onInfraredReceivedNumber(function (num: number) {
         light.setPixelColor(9, 0xff0000)
         console.log(`Room is in gebruik`)
     }
-    if (num === 110) {
-        light.setPixelColor(8, 0xffff00)
-        roomtimer++
-    }
-    if (num === 101) {
-        light.setPixelColor(8, 0x00ffff)
-        roomtimer--
+    if (num !== 111 && num !== 100) {
+        roomtimer = num
     }
     console.log(roomtimer)
 }) 
+forever(function () {
+    if (closed) {
+        //run heartRateCode
+    }
+})
